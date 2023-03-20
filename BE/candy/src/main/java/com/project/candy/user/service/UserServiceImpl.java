b273735e1fb5d2b0d4b00cb87b1fdc65c56afab6
hyunho <potato3884@naver.com>
2023-03-20 17:54:13 +0900

    FEAT : 유저 엔티티 관련 컨트롤러 서비스 레포지토리 작성 , Exception  작성

---
+++ BE/candy/src/main/java/com/project/candy/user/service/UserServiceImpl.java
@@ -0,0 +1,44 @@
+package com.project.candy.user.service;
+
+import com.project.candy.user.dto.CreateUserRequest;
+import com.project.candy.user.entity.Role;
+import com.project.candy.user.entity.User;
+import com.project.candy.util.BaseEntity;
+import org.springframework.stereotype.Service;
+
+/**
+ * packageName    : com.project.candy.user.service
+ * fileName       : UserServiceImpl
+ * date           : 2023-03-20
+ * description    : User 정보를 다루는 Service의 구현체
+ */
+
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public String print_home() {
+
+        return "null";
+    }
+
+    @Override
+    public void CreateUser(CreateUserRequest createUserRequest) {
+        // 받아온 이메일 값이 중복되는지 확인
+
+        User user = User.builder()
+                .email(createUserRequest.getEmail())
+                .nickname(createUserRequest.getNickName())
+                .gender(createUserRequest.getGender())
+                .birth(createUserRequest.getBirth())
+                .profileImage("null")
+                .role(Role.GUEST)
+                .baseEntity(BaseEntity.builder()
+                        .constructor(createUserRequest.getEmail())
+                        .isDelete(false)
+                        .updater(createUserRequest.getEmail())
+                        .build())
+                .build();
+    }
+}
