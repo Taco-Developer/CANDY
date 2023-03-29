77eb84724fc37db1dab8aa536485c577ff7400ed
hyunho <potato3884@naver.com>
2023-03-29 17:47:07 +0900

    woww

--- BE/flaskServer/model.py
+++ BE/flaskServer/model.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 import random
 import pymysql
-
+import datetime
 def rename(name):
     if '(' in name:
         return name[ :name.index('(')]
@@ -14,12 +14,12 @@ def rename(name):
 # 만약 해당 유저가 작성한 리뷰가 없다면
 # 그유저가 회원가입때 사용한 선호도 기반으로 새로 추천
 # 각각의 인자들은 'Aroma' 이런형식으로 스트링을 보낸다.
-def reccomend_beer_prefer_base(conn , first , second , third , forth) :
+def reccomend_beer_prefer_base(conn , first , second , third , forth, user_id) :
     sql = "select * from beer"
     sql_beer = pd.read_sql_query(sql,conn)
     print(sql_beer.columns)
     old_cols = [first, second, third,forth]
-
+   
 
     new_cols = []
     new_cols.append(first + '_new')
@@ -43,7 +43,6 @@ def reccomend_beer_prefer_base(conn , first , second , third , forth) :
     sql_beer.insert(0, 'overall_new', overall_list)
     # beer_list = sql_beer.sort_values(by='overall_new')['beer_kr_name'][:10]
     beer_list = sql_beer.sort_values(by='overall_new').head(10)[['beer_kr_name','beer_id']]
-    print(beer_list)
     # data = pd.read_csv('data/정제된데이터.csv', encoding='utf-8', index_col=0) 
     # data['아이디'] = data['아이디'].apply(rename)
     # df = pd.read_csv('data/맥주_종류별_평점.csv', encoding='utf-8', index_col=0) 
@@ -53,7 +52,19 @@ def reccomend_beer_prefer_base(conn , first , second , third , forth) :
     # beer_similarity = cosine_similarity(beer_matrix)
     # beer_similarity = pd.DataFrame(data=beer_similarity, index=beer_matrix.index, columns=beer_matrix.index)
     # print(beer_similarity.head(10))
+    cur_now=datetime.datetime.now()
+     
+    cur_now = cur_now.strftime("%Y-%m-%d %H:%M:%S")+".000000"
+    print(" cur _now :  ",cur_now)
+    print("user_id  :   ",user_id)
+    sql ="INSERT INTO candy.recommendation_candy(created_at, updated_at, constructor, is_delete, updater,beer_id_list , user_id) VALUES("+cur_now+","+cur_now+","+"admin@admin.com"+","+"false"+","+ "admin@admin.com"+","+"여기다 리스트 넣으셈"+","+user_id+")"
+    print(sql)
+    # sql_beer = pd.read_sql_query(sql,conn)
     return beer_list
+
+
+def get_similar_users(name, n,user_similarity):
+    return user_similarity.loc[name].sort_values(ascending=False)[:n]
 
 def del_rated_beer(df, user, i):
     # user: 추천을 원하는 사용자
@@ -71,44 +82,47 @@ def reccomend_cf (conn,email,cur_user_id):
         where b.beer_id = r.beer_id and u.user_id = r.user_id"""
     
     review_sql  = pd.read_sql_query(sql,conn)
-    print(review_sql.shape)
+    # print(review_sql.shape)
     beer_matrix = review_sql.pivot_table(index='beer_en_name', columns='email', values='overall')
 
     beer_matrix.fillna(0, inplace=True)
     id_matrix = beer_matrix.transpose()
     user_similarity = cosine_similarity(id_matrix)
     user_similarity = pd.DataFrame(data=user_similarity, index=id_matrix.index, columns=id_matrix.index)
+    users = get_similar_users(email, 2,user_similarity).index[1:2]
+    
+    rcmmd_beer=[]
+    print("함수 돌린 값 : ",users)
+    print("끝")
+    # print(beer_matrix.reset_index().head())
+    # print(del_rated_beer(beer_matrix.reset_index(), 'ac@naver.com', '1776@review.com').sort_values('1776@review.com'  , ascending=False)['beer_en_name'][:10])
+    for i in users:
+        # 이미 별점을 매긴(마셔본) 맥주 제외
+        # 유사도가 높은 사용자가 높게 별점을 준 맥주 리스트 받아옴
+        print("함수 돌린 값 : ",i)
+        print("끝")
+        beer_list = del_rated_beer(beer_matrix.reset_index(), email, i)
+        beer_list = beer_list.sort_values(i, ascending=False)['beer_en_name'][:10]
+        print(beer_list)
+        # rcmmd_beer.append(beer_list)
 
-    print(user_similarity.head())
     return "협업 필터링 작업완료"
 
 # email 을 받으면 해당 유저에 대한 candy로직을 분기해주는 메소드
 def reccomend_candy(conn,email) :
-    
+    sql="select user_id , email from user"
+    user = pd.read_sql_query(sql,conn)
+    cur_user_id  = user.loc[user['email']==email]['user_id']
+    print("cur_user_id : ",cur_user_id)
+    print("cur_email : ",email)
     sql = "select *  from candy.review r , candy.user u where r.user_id = u.user_id and u.email= "+'"'+email+'"' 
     review_beer = pd.read_sql_query(sql,conn)
     if review_beer.empty :
-        reccomend_beer_prefer_base(conn , 'aroma','appearance' , 'flavor','mouthfeel')
+        print("선호도 맥주 : " ,reccomend_beer_prefer_base(conn , 'aroma','appearance' , 'flavor','mouthfeel',cur_user_id))
     else :
-        sql="select user_id , email from user"
-        user = pd.read_sql_query(sql,conn)
-        cur_user_id  = user.loc[user['email']==email]['user_id']
         print(reccomend_cf(conn,email,cur_user_id))
     return "분기완료"
 
-# data = pd.read_csv('data/정제된데이터.csv', encoding='utf-8', index_col=0) # 맥주 리뷰 데이터 전처리 후
-# data['아이디'] = data['아이디'].apply(rename)
-# user_list = list(data['아이디'].unique())
-# # print(data.head())
-# unique_name_city_df = data[['아이디']].drop_duplicates().reset_index(drop=True)
-# n_rows = len(unique_name_city_df)
-# new_col = [f"{i+1}@review.com" for i in range(n_rows)]
-
-# unique_name_city_df['email'] = new_col
-# unique_name_city_df['created_at']= "2023-03-22 09:56:24.00000"
-# unique_name_city_df['updated_at']= "2023-03-22 09:56:24.00000"
-# unique_name_city_df['constructor']="admin@admin.com"
-# print(unique_name_city_df)
 conn = pymysql.connect(
     host='j8b105.p.ssafy.io',
     port=8306,
@@ -117,7 +131,7 @@ conn = pymysql.connect(
     db='candy',
     charset="utf8"
 )
-reccomend_candy(conn, 'ac@naver.com')
+reccomend_candy(conn, '2700411378@candy.com')
 # print(reccomend_beer_prefer_base(conn , 'aroma','appearance' , 'flavor','mouthfeel'))
 conn.close()
 
